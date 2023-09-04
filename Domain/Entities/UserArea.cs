6b1d7bdfb418706f9cf3a1a6cb545fd1fa61fe16
EstebanSansart <julianestebansanabria@gmail.com>
2023-09-04 18:01:01 -0500

    feat: :memo: ForeignKeysAdded
    Foreign keys added, Context created

--- Domain/Entities/UserArea.cs
+++ Domain/Entities/UserArea.cs
@@ -1,5 +1,8 @@
 namespace Domain.Entities;
 public class UserArea
 {
-    
+    public int IdUserFk { get; set; }
+    public User User { get; set; }
+    public int IdAreaFk { get; set; }
+    public Area Area { get; set; }
 }
--- Persistence/ApiIncidenciasIContext.cs
+++ Persistence/ApiIncidenciasIContext.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence;
@@ -5,5 +7,27 @@ public class ApiIncidenciasIContext : DbContext
 {
     public ApiIncidenciasIContext(DbContextOptions<ApiIncidenciasIContext> options) : base(options)
     {
+    }
+    public DbSet<Area> Areas { get; set; }
+    public DbSet<Contact> Contacts { get; set; }
+    public DbSet<ContactCategory> ContactCategories { get; set; }
+    public DbSet<ContactType> ContactTypes { get; set; }
+    public DbSet<DocType> DocTypes { get; set; }
+    public DbSet<Incidence> Incidences { get; set; }
+    public DbSet<IncidenceDetail> IncidenceDetails { get; set; }
+    public DbSet<IncidenceLevel> IncidentLevels { get; set; }
+    public DbSet<IncidenceType> IncidenceTypes { get; set; }
+    public DbSet<Place> Places { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<State> States { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<WorkTool> WorkTools { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UserArea>().HasKey(r => new{r.IdUserFk, r.IdAreaFk});
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
     }
 }
